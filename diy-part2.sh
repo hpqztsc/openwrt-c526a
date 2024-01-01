fa30ded05f18d325ddb98af8636b84ed7e41ca04
hpqztsc <49110606+hpqztsc@users.noreply.github.com>
2024-01-01 19:31:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,17 +9,17 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+
+
 # 管理地址
-sed -i 's/192.168.1.1/192.168.99.3/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
 # 默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
-
-# 修改NAS为网络存储
-sed -i 's/"NAS"/"网络存储"/g' `grep "NAS" -rl ./`
-
-# 修改固件默认时区
-sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
-
-# 修改固件默认名称
-sed -i 's/ImmortalWrt/HIWIFI/g' package/base-files/files/bin/config_generate
+rm -rf feeds/kenzo/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci/Makefile
