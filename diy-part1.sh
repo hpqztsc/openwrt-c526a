abe734a61250f143022eaaedce49341e07489aec
hpqztsc <49110606+hpqztsc@users.noreply.github.com>
2024-01-01 20:58:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,8 +17,8 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # 下载新主题
-rm -rf ./package/lean/autocore
-git clone -b lede https://github.com/supsupsuperstar/autocore.git package/lean/autocore
+# rm -rf ./package/lean/autocore
+# git clone -b lede https://github.com/supsupsuperstar/autocore.git package/lean/autocore
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge
