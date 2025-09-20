0a6dfc7299de18b823401d3201dc2f12c17b5a9c
ztsc <hpqztsc@foxmail.com>
2025-09-20 03:58:38 +0000

    add new yml

--- lede-diy2.sh
+++ lede-diy2.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
-sed -i 's/192.168.1.28/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.29/g' package/base-files/files/bin/config_generate
 
 # 版本号里显示一个自己的名字（ztsc build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /ztsc build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
